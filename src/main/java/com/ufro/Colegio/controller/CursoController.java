c86c70c79372d4ab9508e9188fe07a4fc489a6df
isialbayay <i.albayay01@ufromail.cl>
2022-06-12 07:02:38 -0400

    fin de integración

--- src/main/java/com/ufro/Colegio/controller/CursoController.java
+++ src/main/java/com/ufro/Colegio/controller/CursoController.java
@@ -1,13 +1,15 @@
 package com.ufro.Colegio.controller;
 
+import com.ufro.Colegio.model.Alumno;
 import com.ufro.Colegio.model.Curso;
+import com.ufro.Colegio.repository.AlumnoRepository;
 import com.ufro.Colegio.repository.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 
 @Controller
@@ -15,6 +17,10 @@ public class CursoController {
 
     @Autowired
     CursoRepository cursoRepository;
+
+    @Autowired
+    AlumnoRepository alumnoRepository;
+
     @RequestMapping("/registrar-curso")
     public String registrarCurso(){
         return "registrarCurso";
@@ -29,5 +35,13 @@ public class CursoController {
     public String verCursos(Model model){
         model.addAttribute("cursos", cursoRepository.findAll());
         return "verCursos";
+    }
+
+    @GetMapping("/ver-curso")
+    public String verCurso(@RequestParam(value="id") Long id, Model model){
+        Optional<Curso> curso=cursoRepository.findById(id);
+        model.addAttribute("curso",curso.orElseThrow());
+        model.addAttribute("alumnos", alumnoRepository.findAllByCursoId(id));
+        return "verCurso";
     }
 }
--- src/main/java/com/ufro/Colegio/repository/AlumnoRepository.java
+++ src/main/java/com/ufro/Colegio/repository/AlumnoRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface AlumnoRepository extends CrudRepository<Alumno, Long> {
 
+    Iterable<Alumno> findAllByCursoId(Long ItemId);
+
 }
