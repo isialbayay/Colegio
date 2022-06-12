95825ccd0875ad2acae04fd44281abf2f64f30de
isialbayay <i.albayay01@ufromail.cl>
2022-06-12 04:41:42 -0400

    Correcciones en clase Alumno

--- src/main/java/com/ufro/Colegio/model/Alumno.java
+++ src/main/java/com/ufro/Colegio/model/Alumno.java
@@ -1,7 +1,6 @@
 package com.ufro.Colegio.model;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
 
 @Entity
 public class Alumno {
@@ -11,14 +10,14 @@ public class Alumno {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "matricula" nullable = false, length = 15 )
+    @Column(name = "matricula", nullable = false, length = 15 )
     private String matricula;
 
     @Column(name = "Nombre", nullable = false, length = 150 )
     private String mensaje;
 
-    @Column(name = "Nombre", nullable = false, length = 150 )
-    private String mensaje;
+    @Column(name = "imagen", nullable = false, length = 150 )
+    private String imagen;
 
     @Column(name = "Apoderado", nullable = false, length = 100)
     private String nombre;
